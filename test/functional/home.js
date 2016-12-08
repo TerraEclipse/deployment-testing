356cbc173b3a51c3b7f533aa69b094a5327d60eb
Brian Link <cpsubrian@gmail.com>
2016-12-08 14:18:31 -0800

    Changes the home message.

--- test/functional/home.js
+++ test/functional/home.js
@@ -3,12 +3,12 @@ const server = require('../../server')
 const lab = exports.lab = require('lab').script()
 const {describe, it} = lab
 
-describe('functional tests - hello', () => {
-  it('should get the hello response', (done) => {
+describe('functional tests - home', () => {
+  it('should get the welcome response', (done) => {
     server.inject({method: 'GET', url: '/'}, (res) => {
       assert(res.statusCode, 200)
       assert(res.headers['content-type'], 'application/json; charset=utf-8')
-      assert.deepEqual(res.result, {message: 'Hello World!'})
+      assert.deepEqual(res.result, {message: 'Welcome!'})
       done()
     })
   })
