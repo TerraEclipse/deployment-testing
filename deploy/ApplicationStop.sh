2c2c4760d5ad762bc8bfe0670bd9c1183744de5e
Brian Link <cpsubrian@gmail.com>
2016-12-17 12:50:27 -0800

    Oops, wrong hook fixed.

--- deploy/ApplicationStop.sh
+++ deploy/ApplicationStop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'ApplicationStop'
 
-# Stop application via upstart.
-service deployment-testing stop
+# Stop application via upstart (don't care if it works).
+service deployment-testing stop || true
